ae5def0934a97d454a987f73e2096eb5f20d5166
d1Ng0 <diegotrazzi@gmail.com>
2018-11-17 21:56:35 -0800

    wip getting the bot back to work ...

--- market_maker/utils/log.py
+++ market_maker/utils/log.py
@@ -7,7 +7,7 @@ def setup_custom_logger(name, log_level=settings.LOG_LEVEL):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
     logging.Formatter.converter = time.gmtime # we want to use UTC time, to keep consistency across platforms and timezones
 
-    handler = logging.StreamHandler()
+    handler = logging.StreamHandler() # sends logging output to streams such as sys.stdout, sys.stderr or any file-like object
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
